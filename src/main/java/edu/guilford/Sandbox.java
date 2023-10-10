9ce6e0c0054db9fffba79b72982cdf0049b73608
Adhvika Karthik <AdhvikaK@gmail.com>
2023-10-10 10:12:01 -0400

    looping

--- src/main/java/edu/guilford/Sandbox.java
+++ src/main/java/edu/guilford/Sandbox.java
@@ -1,8 +1,32 @@
 package edu.guilford;
 
+import java.util.Scanner;
+
 public class Sandbox {
     public static void main(String[] args) {
         // A place for testing Java code
+        //counting loop
+        //suppose we deposit $1000 in a savings account. the account has an annual interest rate
+        // of 3% and you add $100 at the end of each year
+        //each year for the next 20 years
+
+        //Initialize variables: initialDeposit, interestRate, yearlyDeposit, currentValue
+        double initialDeposit = 1000;
+        double interestRate = .03;
+        double yearlyDeposit = 100;
+        double currentValue = initialDeposit;
+
+        //Counting loops need an *index*: what iteration of the loop is the programming running?
+        int year = 0; //we start at year 0
+        int finalYear = 20; //when we stop the loop
         
+        while (year < finalYear) {
+            //keep running as long as we haven't hit finalYear
+            currentValue = currentValue * (1 + interestRate) + yearlyDeposit;
+            System.out.println("At the end of year " + year + ", value =$ " + String.format("%.2f", currentValue));
+            year = year + 1; //make sure we move to the next year
+
+
+        }
     }
 }
